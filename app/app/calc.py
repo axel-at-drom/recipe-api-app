c57e28ba114fb49b16be4d01ca91e25bb608f68e
axel-at-drom <axel@drom.solutions>
2022-12-06 15:56:33 +0100

    lesson 42: setup db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """ADd x and y and return the result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result"""
